48f1b3514e309637b9c975ebe9d56037e38d2ca2
ben <186181126+benissimoff@users.noreply.github.com>
2025-06-28 02:34:42 +0300

    feat: add simple app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     application
     checkstyle
     id("org.sonarqube") version "6.2.0.5505"
+
 }
 
 group = "hexlet.code"
@@ -12,11 +13,17 @@ repositories {
     mavenCentral()
 }
 
-
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/io.javalin/javalin
+    implementation("io.javalin:javalin:6.7.0")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.17")
 }
 
 sonar {
